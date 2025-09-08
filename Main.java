7ab609b6e838c9acd7134c17ed4984429582210f
Orlov Sergey <Graforlov163@gmail.com>
2025-09-08 15:40:03 +0400

    Add Main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyHashMap<Integer, String> map = new MyHashMap();
+        MyHashMap<Integer, String> map = new MyHashMap<>();
         map.put(1, "One");
         map.put(2, "Two");
         map.put(3, "Three");
