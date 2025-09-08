77ff7e4c2d8b26e5f31f42e8549395b14a1ec66b
Sergey Orlov <graforlov163gmail.com>
2025-09-08 15:52:49 +0400

    #

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        MyHashMap<Integer, String> map = new MyHashMap<>();
-        map.put(1, "One");
-        map.put(2, "Two");
-        map.put(3, "Three");
-        System.out.println(map);
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+
+        System.out.println(map.get("two"));     // 2
+        System.out.println(map.remove("one"));  // 1
+        System.out.println(map);                // {three=3, two=2}
     }
 }
